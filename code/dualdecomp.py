Fixed solutions to subproblems.  Added scaling to input covariance matrices to the admm loop.

code/dualdecomp.py
@@ -2,82 +2,102 @@ __author__ = 'wayne'
 
 import numpy as np
 from numpy.linalg import eigh
+from numpy.linalg import inv
 from subproblem import solveSub
-
+from copy import copy
 # Matrix Two Norm
-def norm(X):
-    return sum(sum(X * X))
+from numpy.linalg import norm
 
 # Likelihood Prox Step
 def likelihoodStep(Y,U,C,ro):
     temp = ro * (Y - U) - C
     L, Q = eigh(temp)
+    # print "Smallest Eigenvalue", np.min(L)
     for i in range(len(L)):
         li = L[i]
         L[i] = (li + np.sqrt(li*li + 4.0*ro))/(2.0*ro)
+    # print "Smallest Eigenvalue", np.min(L)
+
     return Q.dot(np.diag(L).dot(Q.transpose()))
 
 # Z is an n x n numpy array (row sliced)
 # alpha, ro are positive constants
-def dualDecomp(Z,alpha,ro,eta,h):
+def dualDecomp(Z,V,alpha,ro,eta,h):
     X = Z
     n = len(Z)
-    V = np.zeros((n,n))
+    temp = V
     for i in range(n):
-        X[i] = solveSub(Z[i],V[i],i,alpha,ro,h)
-    V += eta * (X - X.transpose())
-    error = norm(X-X.transpose())
-    counter = 1.0
-    while(error > 0.0001):
-
-        print counter, error
+        X[i] = solveSub(Z[i], V[i], i, alpha, ro, h)
+    temp += eta * (X - X.transpose())
+    error = norm(X - X.transpose())
+    counter = 0.0
+    while(error > 0.00001):
         for i in range(n):
-            X[i] = solveSub(Z[i],V[i],i,alpha,ro,h)
-        V += eta * (X - X.transpose())
+            X[i] = solveSub(Z[i], V[i], i, alpha, ro, h)
+        temp += eta * (X - X.transpose())
         error = norm(X - X.transpose())
         counter += 1.0
+        # print counter
+
+    X = np.around(X,15)
     X = 0.5 * (X + X.transpose())
-    return X
+
+    return X, temp
 
 # Main ADMM Loop
-def admm(C,alpha,ro,eta,h,eps):
-    n = len(C)
-    Y = np.eye(n)
-    U = np.zeros((n,n))
-    print Y
-    X = likelihoodStep(Y,U,C,ro)
-    print X
-    Y = dualDecomp(X-U, alpha,ro,eta,h)
-    U = U + ro*(X - Y)
+def admm(Cov, alpha, mu, eta, h, eps):
 
-    Y_prev = np.eye(n)
+    covNormalizer = np.sqrt(np.diag(Cov))
+    C = Cov / np.outer(covNormalizer, covNormalizer)
+    # maxOffDiag = np.max(np.abs(np.tril(C, -1)))
+    # C = np.array(C / maxOffDiag)
+    ro = mu
+    n = len(C)
+    Y = inv(alpha*np.eye(n) + C)
+
+    U = np.zeros((n,n))
+    V = np.zeros((n,n))
+    # Y, V = dualDecomp(U+X, np.zeros((n,n)), alpha, ro, eta, h)
+    # U = U + X - Y
 
     iter = 0
-    while(norm(X-Y) > eps or norm(Y-Y_prev) > eps):
-        Y_prev = Y
+    error1 = 1
+    error2 = 1
+    while(error1 > eps or error2 > eps):
+        print iter, error1, error2
+        Y_prev = copy(Y)
         iter += 1
-        print iter
-        print X-U
-        print Y
-        X = likelihoodStep(Y,U,C,ro)
-        Y = dualDecomp(U-X, alpha,ro,eta,h)
-        U = U + ro*(X - Y)
+        # X = likelihoodStep(Y,U,C,ro)
+        L, Q = eigh(ro * (Y - U) - C)
+        # while np.min(L) < 0:
+        #     ro *= 0.5
+        #     L, Q = eigh(ro * (Y - U) - C)
+        #     print np.min(L), np.max(L), np.linalg.cond(ro*(Y-U) - C)
+        L = (L + np.sqrt(L*L + 4.0*ro))/(2.0*ro)
+        X = Q.dot(np.diag(L).dot(Q.transpose()))
+        Y, V = dualDecomp(U+X, V, alpha, ro, eta, h)
+        U = U + X - Y
+        error1 = norm(X-Y)
+        error2 = norm(Y-Y_prev)
 
     return Y
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-from numpy.random import multivariate_normal as mvn
-from numpy.linalg import inv
-Theta = np.diag([1.5]*5)
-for i in range(1,5):
-    Theta[0][i] = -0.2
-    Theta[i][0] = -0.2
-Sigma = inv(Theta)
+    from numpy.random import multivariate_normal as mvn
+    from numpy.linalg import inv
+    Theta = np.diag([1.5]*5)
+    for i in range(1,5):
+        Theta[0][i] = -0.2
+        Theta[i][0] = -0.2
+    Sigma = inv(Theta)
 
-sample = mvn(np.zeros((5)), Sigma, 1000)
-
-C = np.cov(sample.transpose())
-
-admm(C,0.1,0.5,0.9,lambda x: abs(x),0.00001)
+    sample = mvn(np.zeros((5)), Sigma, 1000)
+    center = sample - np.mean(sample)
+    for i in range(5):
+        center[:,i] /= norm(center[:,i])
+    # print Sigma
+    C = center.transpose().dot(center)
+    for alpha in [0.01,0.05,0.1,0.5]:
+        print admm(C,alpha,0.5,0.9,lambda x: abs(x),0.00001)

code/glasso.py
@@ -10,7 +10,7 @@ def onenorm(x):
 ### Two norm
 # x is any numpy.matrix
 def twonorm(x):
-    return np.sqrt(sum(np.square(x)))[0,0]
+    return np.sqrt(np.sum(np.square(x)))
 
 ### LASSO Objective (admm form)
 # X is an N x p matrix
@@ -32,7 +32,7 @@ def admmObj(X,y,beta,z,lambduh,rho,a):
 # rho is a positive number
 # output is a p x 1 matrix
 def admm(X,y,lambduh,rho):
-    tol = 1e-100
+    tol = 1e-10
     p = X.shape[1]
     xtxpi = (X.T * X + rho * np.matrix(np.identity(p))).I
     oldbeta = np.matrix([[0.0]]*p)
@@ -40,8 +40,8 @@ def admm(X,y,lambduh,rho):
     z = np.matrix([[1.0]]*p)
     a = np.matrix([[1.0]]*p)
     iteration = 0
-    while iteration < 100:
-        oldbeta = beta
+    while iteration < 100 and twonorm(oldbeta - beta) > tol:
+        oldbeta = copy(beta)
         beta = xtxpi * X.T * y + rho * xtxpi  * z -  xtxpi *a
         for i in range(p):
             z[i,0] = np.sign(beta[i,0] + 1.0/rho*a[i,0])*max(0.0,(np.absolute(beta[i,0] + 1.0/rho*a[i,0])-lambduh[0,i]/rho))
@@ -106,6 +106,9 @@ if __name__ == "__main__":
     lambduh= np.matrix(np.array([[0.1]*5]*5))
 
     out = glassoSolve(Cov,lambduh,1.0)
+
+    print np.around(out,3)
+
 
 
 

code/subproblem.py
@@ -4,22 +4,43 @@ import numpy as np
 from disjointset import disjointSet
 
 # z,v are 1d numpy arrays
+# l is
 def solveSub(z,v,l,alpha,ro,h):
+    # In Defazio and Caetano, the description of the algorithm maintains
+    # a sorted to original function throughout the algorithm.
+    # I found it is much easier to parse under the assumption that the subproblem
+    # receives a presorted array and then revert the orderings after the problem is solved.
 
     n = len(z) - 1
     w = z - v
     w = np.delete(w,l)
-    mu = np.argsort(abs(w))
+    indices = np.argsort(np.abs(w))
+    sortedToOriginal = {indices[::-1][i]:i for i in range(len(indices))}
+
+    x = _solveSub(np.abs(w)[indices[::-1]], alpha, ro, h)
+    x = np.array([x[sortedToOriginal[k]] for k in range(n)])
+
+    for k in range(len(x)):
+        if x[k] < 0:
+            x[k] = 0.0
+        if w[k] < 0:
+            x[k] *= -1.0
+
+    x = np.insert(x,l,z[l])
+    return x
+
+
+
+def _solveSub(w,alpha,ro,h):
+    n = len(w)
     values = [0.0] * n
 
     for k in range(len(values)):
-        j = mu[k]
-        values[j] = abs(w[j]) - alpha/ro*(h(j+1) - h(j))
+        values[k] = np.abs(w[k]) - alpha/ro*(h(k+1) - h(k))
 
     set = disjointSet(n,values)
 
     for k in range(len(values)):
-        j = mu[k]
         r = k
         while r > 1 and set.findValue(r) >= set.findValue(r-1):
             set.union(r,r-1)
@@ -29,23 +50,34 @@ def solveSub(z,v,l,alpha,ro,h):
     x = np.array([0.0] * n)
     for k in range(len(values)):
         x[k] = set.findValue(k)
-        if x[k] < 0:
-            x[k] = 0.0
-        if w[k] < 0:
-            x[k] *= -1.0
-    x = np.insert(x,l,z[l])
+
     return x
 
+
 if __name__ == "__main__":
-    z = np.array([1,1,1])
-    v = np.array([0,0,0])
+    z = np.array([0,-1,2,-3,4,-5,6])
+    v = np.array([0]*7)
     l = 0
-    alpha = 0.5
-    ro = 0.9
+    alpha = 1.0
+    ro = 1.0
     def h(x):
         return abs(x)
     import cProfile
-    cProfile.run("""for i in range(10000):
-        solveSub(z,v,l,alpha,ro,lambda x: np.log(x+1))
-    """)
-    print solveSub(z,v,l,alpha,ro,lambda x: np.log(x+1))
+    # cProfile.run("""for i in range(100):
+    #     solveSub(z,v,l,alpha,ro,lambda x: np.log(x+1))
+    # """)
+
+    import numpy.random as rnd
+    w = rnd.random_integers(0,100,size=(60))
+
+    indices = np.argsort(w)
+
+    def h(x):
+        return np.abs(x+1)
+    #
+    # cProfile.run("""for i in range(6000):
+    #     _solveSub(w[indices[::-1]],1.0,1.0,h)
+    #     """)
+
+    print np.around(solveSub(z,v,l,alpha,ro,lambda x: np.log(x+1)),2)
+    print z